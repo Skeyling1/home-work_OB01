d68b1abcbab46885830ac819d48c6897191e8af8
Skeyling1 <skeyling1@gmail.com>
2024-06-05 21:23:32 +0300

    first_code

---
+++ main.py
@@ -0,0 +1,20 @@
+#Менеджер задач
+#Задача: Создай класс Task, который позволяет управлять задачами (делами).
+# У задачи должны быть атрибуты: описание задачи, срок выполнения и статус (выполнено/не выполнено).
+# Реализуй функцию для добавления задач, отметки выполненных задач и вывода списка текущих (не выполненных) задач.
+
+
+class Task():
+    def __int__(self, description, dead_line, state):
+        self.description = description
+        self.dead_line = dead_line
+        self.state = state
+
+    def add_task(self, description):
+        pass
+
+    def done_mark(self, ):
+        pass
+
+    def do_list(self):
+        print()
