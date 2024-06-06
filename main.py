ee479f552f25e8ad2241452b1c16442cc921705c
Skeyling1 <skeyling1@gmail.com>
2024-06-06 21:41:00 +0300

    Update main.py

--- main.py
+++ main.py
@@ -11,8 +11,8 @@ class Task:
         self.accomp = accomp
 
     def add_task(self):
-        self.description = input("введите задачу")
-        self.dead_line = input("введите срок выполнения")
+        self.description = input("введите задачу: ")
+        self.dead_line = input("введите срок выполнения: ")
         self.accomp = False
 
     def done_mark(self):
@@ -21,10 +21,22 @@ class Task:
 tasks = []
 
 while True:
-    print("введите 0 для добавления задачи")
-    print("введите 1 для отметки о выполнении")
-    print("введите 2 для просмотра задачь")
-    print("нажмите любую другую кнопку для выхода")
+    print("текущие задачи:")
+    if tasks:
+        s = 1
+        for i in tasks:
+            if i.accomp != True:
+                print("задача №", s, ":  ", i.description, "\t", end="")
+                print("закончить до:  ", i.dead_line)
+                s += 1
+    else: print("нет задач")
+
+
+
+    print("\nвведите 0 для добавления задачи ")
+    print("введите 1 для отметки о выполнении ")
+    print("введите 2 для просмотра актуальных задач ")
+    print("нажмите любую другую кнопку для выхода ")
     comand = input()
     if comand == "0":
         a = Task(True, True, True)
@@ -35,7 +47,15 @@ while True:
     elif comand == "1":
         pass
     elif comand == "2":
-        print(tasks)
+        s = 1
+        for i in tasks:
+            if i.accomp != True:
+                print("задача №", s, ":  ", i.description, "\t", end="")
+                print("закончить до:  ", i.dead_line)
+                s += 1
+
+
+
     else: break
 
 
